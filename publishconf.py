564d57e01387c0b0bc547a30241c5a380d49cdae
Andre Martins <andre.t.martins@gmail.com>
2018-05-26 22:10:03 +0100

    Removed links.

--- publishconf.py
+++ publishconf.py
@@ -11,10 +11,10 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://deep-spin.github.io'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
